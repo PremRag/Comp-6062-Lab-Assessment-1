d8ccbb97d5cdb38a1984535dfa173f7d9aab038f
PremRag <Prem143rag0@gmail.com>
2025-02-13 18:59:08 -0500

    Lab Assessment - 1
    Added index.html, styles.css, app.js

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+
+const studentFullName   = 'Gudishala Prem Rag';
+const studentNumber     = 1198030;
+const result            = concat(studentFullName, - ,studentNumber);
+console.log(result);    //this will print both student name and number uisng only single code line.
+const headerContent     =document.querySelector(h1)
