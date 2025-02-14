2f01f5151386e5fb5e931195c03d813810b5d1d1
PremRag <Prem143rag0@gmail.com>
2025-02-13 19:16:22 -0500

    app.js update 2
    added latest update

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,13 @@
 
 const studentFullName   = 'Gudishala Prem Rag';
 const studentNumber     = 1198030;
-const result            = concat(studentFullName, - ,studentNumber);
+
+const result            = `${studentFullName} - ${studentNumber}`;
+
 console.log(result);    //this will print both student name and number uisng only single code line.
-const headerContent     = document.querySelector(h1)
-innerHTML 
+
+const headerContent     = document.querySelector("h1");
+
+headerContent.innerHTML = `${studentFullName} - ${studentNumber}`;
+
+headerContent.classList.add("headingPrimary");
