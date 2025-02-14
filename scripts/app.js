36eb77b2ce97a71496a321fe4365335d10453a63
PremRag <Prem143rag0@gmail.com>
2025-02-13 19:01:33 -0500

    Update app.js
    updated some java code

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,5 @@ const studentFullName   = 'Gudishala Prem Rag';
 const studentNumber     = 1198030;
 const result            = concat(studentFullName, - ,studentNumber);
 console.log(result);    //this will print both student name and number uisng only single code line.
-const headerContent     =document.querySelector(h1)
+const headerContent     = document.querySelector(h1)
+innerHTML 
